f3f65d39357ff56b95c7e56bf3c976d70b7a693c
Dennis Wei <dw2654@columbia.edu>
2025-12-06 07:54:32 -0500

    change to general

---
+++ 2025/aoc_elixir/solutions/6.exs
@@ -0,0 +1,24 @@
+defmodule Day6 do
+  def get_input(test \\ false) do
+    filename = case test do
+      false -> "input/6.txt"
+      true -> "test_input/6.txt"
+    end
+    Input
+      # .ints(filename)
+      # .line_tokens(filename)
+      # .lines(filename)
+      # .line_of_ints(filename)
+      # .ints_in_lines(filename)
+  end
+
+  def solve(test \\ false) do
+    input = get_input(test)
+    part1 = nil
+    part2 = nil
+    IO.puts("Part 1: #{part1}")
+    IO.puts("Part 2: #{part2}")
+  end
+end
+
+Day6.solve()
